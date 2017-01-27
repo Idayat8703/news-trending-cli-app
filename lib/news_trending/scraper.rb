cabb9bd103d821afc3c9056b9da888ae552e8217
Idayat Oshobajo <idayat.oshobajo@gmail.com>
2017-01-27 04:00:50 +0000

    fix runtime error

--- lib/news_trending/scraper.rb
+++ lib/news_trending/scraper.rb
@@ -1,19 +1,20 @@
 class NewsTrending::Scraper
 
-  def self.scrape_nytimes
-    doc = Nokogiri::HTML(open("http://www.nytimes.com/pages/technology/index.html"))
+  # scrapes and returns intial info about the most recent news from various sections on the nytimes website
+  def self.scrape_nytimes1
+    doc = Nokogiri::HTML(open("https://www.nytimes.com/section/technology?WT.nav=page&action=click&contentCollection=Tech&module=HPMiniNav&pgtype=Homepage&region=TopBar"))
 
     article = NewsTrending::Article.new
-    article.name = doc.search("h3").first.text.strip
-    article.author = doc.search("h6.byline").first.text.strip
+    article.name = doc.search(".headline").first.text.strip
+    article.author = doc.search(".byline").first.text.strip
     article.summary = doc.search("p.summary").first.text.strip
-    article.url = doc.search("h3 a").first.attr("href").strip
+    article.url = doc.search("h2 a").first.attr("href").strip
     article.source = "The New York Times"
     article
   end
 
   def self.scrape_nytimes2
-    doc = Nokogiri::HTML(open("http://www.nytimes.com/pages/politics/index.html"))
+    doc = Nokogiri::HTML(open("http://www.nytimes.com/pages/politics/index.html?action=click&pgtype=Homepage&region=TopBar&module=HPMiniNav&contentCollection=Politics&WT.nav=page"))
     article = NewsTrending::Article.new
     article.name = doc.search("h3").first.text.strip
     article.author = doc.search("h6.byline").first.text.strip
@@ -35,7 +36,7 @@ class NewsTrending::Scraper
   end
 
   def self.scrape_nytimes4
-    doc = Nokogiri::HTML(open("http://www.nytimes.com/pages/business/index.html"))
+    doc = Nokogiri::HTML(open("http://www.nytimes.com/pages/business/index.html?action=click&pgtype=Homepage&region=TopBar&module=HPMiniNav&contentCollection=Business&WT.nav=page"))
     article = NewsTrending::Article.new
     article.name = doc.search("h3").first.text.strip
     article.author = doc.search("h6.byline").first.text.strip
